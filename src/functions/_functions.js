2caf12d9fea9545a2751e9cb3ad363220d900320
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-21 20:17:47 +0200

    Add scrollToElementById function

--- src/functions/_functions.js
+++ src/functions/_functions.js
@@ -13,3 +13,38 @@ export const formatToTwoDecimals = number => {
   // https://www.jacklmoore.com/notes/rounding-in-javascript/
   return Number(Math.round(number + 'e2') + 'e-2')
 }
+
+export const scrollToElementById = (id, options = {}) => {
+  //Finds y value of given object
+  //https://stackoverflow.com/a/11986374
+
+  /*
+   * Scrolls the page to an element based on its id
+   * @param id String the id of the element to scroll to
+   * @return undefined
+   */
+
+  const defaultOptions = {
+    topMargin: 10, // margin from the top of the viewport in pixels
+  }
+
+  const settings = { ...defaultOptions, ...options }
+
+  const { topMargin } = settings
+
+  const targetElement = document.getElementById(id)
+
+  if (!targetElement) throw new Error(`No element with id ${id} was found.`)
+
+  function findPos(obj) {
+    let curtop = 0
+    if (obj.offsetParent) {
+      do {
+        curtop += obj.offsetTop
+      } while ((obj = obj.offsetParent))
+      return [curtop] - topMargin
+    }
+  }
+
+  window.scroll(0, findPos(targetElement))
+}
