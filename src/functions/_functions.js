d8c437ba3c30e4fdd92b7d939dc91ff0ff4a9845
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-21 19:48:26 +0200

    Create formatToTwoDecimals function

--- src/functions/_functions.js
+++ src/functions/_functions.js
@@ -1,8 +1,15 @@
 export const $ = element => document.querySelector(element)
 
 export const formatNumberToEuro = number => {
-  return Intl.NumberFormat('en-us', {
-    style: 'currency',
-    currency: 'EUR',
-  }).format(number)
+  return Number(
+    Intl.NumberFormat('en-us', {
+      style: 'currency',
+      currency: 'EUR',
+    }).format(number)
+  )
+}
+
+export const formatToTwoDecimals = number => {
+  // https://www.jacklmoore.com/notes/rounding-in-javascript/
+  return Number(Math.round(number + 'e2') + 'e-2')
 }
